36eccc0a2903f173d2620134e45ed4b2a719ed4c
TotixT <santigolopezgarcia22@gmail.com>
2023-08-07 17:15:37 -0500

    (No usar) - Solo el dueño original (Yo)

--- arrancar_proyecto.bat
+++ arrancar_proyecto.bat
@@ -1,2 +1,4 @@
 start cmd.exe /k "cd C:\Program Files\MongoDB\Server\5.0\bin && mongod.exe --dbpath C:\data5\db"
 start cmd.exe /k "cd C:\Users\Santiago_LopezGarcia\Desktop\Node\Proyecto_byV_Back && npm start"
+
+@REM No usar, solo funciona para el pc original del que se hizo el proyecto
